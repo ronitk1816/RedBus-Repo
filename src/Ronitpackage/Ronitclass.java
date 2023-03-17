7eff577c6039b5e89968988d72ed15318d85501a
DELL <DELL@DESKTOP-59I1R6I>
2023-03-17 18:17:50 +0530

    first commit

---
+++ src/Ronitpackage/Ronitclass.java
@@ -0,0 +1,29 @@
+package Ronitpackage;
+
+public class Ronitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Welcome to the Java Course :");
+		
+		System.out.println("Jobbers Cafe");
+		
+		boolean B= false;
+		char ch= 44;
+		byte b= 122;
+		short sh= 12354;
+		int i= 1234567891;
+		long l= 12345678910123l;
+		double d=1.245612366588;
+		float f= 3.22556699f;
+		
+		System.out.println("boolezan "+B);
+		System.out.println("char " +ch);
+		System.out.println("short " +sh);
+		System.out.println("long " +l);
+		System.out.println("double "+d);
+		
+	} 
+      
+}
